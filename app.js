4ff036a641ceb62ba2023b9c342a56253d9e36aa
DEEPAK PRAKASH <calisttoha@gmail.com>
2023-03-15 19:11:31 +0530

    added auth

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const path = require('path')
+
+const session = require('express-session')
+const MongoSession = require('connect-mongodb-session')(session)
+
 const routes = require('./routes/routes')
+const authRoutes = require('./routes/auth')
 
 const db = require('./data/database') 
 const app = express()
 app.use(express.urlencoded({extended:false}))
+
+const MongoStoreSession = new MongoSession({
+    uri:'mongodb://127.0.0.1:27017',
+    collection:'session'
+})
 
 app.use(express.static('public'))
 
 app.set('view engine','ejs')
 
 app.set('views',path.join(__dirname,'views'))
+
+app.use(session({
+    secret:'superSecret',
+    saveUninitialized:false,
+    resave:false,
+    store:MongoStoreSession
+}))
+
+app.use(authRoutes)
 
 app.use(routes)
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',function(req,res) {
-    res.redirect('/products')
+    res.redirect('/shop')
 })
 
-router.get('/products',function(req,res) {
+router.get('/shop',function(req,res) {
     res.render('products')
 })
+router.get('/cart',function(req,res) {
+    res.render('cart')
+})
+router.get('/details',function(req,res) {
+    res.render('details')
+})
+
 
 module.exports = router
